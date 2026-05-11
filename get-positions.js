27e5662a01a18958243c39c189532789314e240e
Todd Adam <66918642+zxc1058438943@users.noreply.github.com>
2026-05-11 14:38:28 +0800

    Create get-positions.js

---
+++ get-positions.js
@@ -0,0 +1,90 @@
+[
+  {
+    "wallet": "0x2005d16a84ceefa912d4e380cd32e7ff827875ea",
+    "walletName": "0x2005...75ea",
+    "enabled": false,
+    "mode": "global",
+    "amountUsd": 1.5,
+    "maxPriceDiff": 0.05,
+    "note": "",
+    "updatedAt": "2026-05-07T07:43:30.794Z",
+    "createdAt": "2026-05-07T07:40:55.877Z"
+  },
+  {
+    "wallet": "0x3d3ca331c493bed9208e242af7589ff73ef415ed",
+    "walletName": "0x3d3c...15ed",
+    "enabled": true,
+    "mode": "live",
+    "amountUsd": 1,
+    "maxPriceDiff": 0.075,
+    "note": "",
+    "updatedAt": "2026-05-08T15:40:12.841Z",
+    "createdAt": "2026-05-06T18:53:38.842Z"
+  },
+  {
+    "wallet": "0x53757615de1c42b83f893b79d4241a009dc2aeea",
+    "walletName": "0x53757615de1c42b83f893b79d4241a009dc2aeea",
+    "enabled": true,
+    "mode": "live",
+    "amountUsd": 1.06,
+    "maxPriceDiff": 0.065,
+    "note": "",
+    "updatedAt": "2026-05-11T06:21:13.327Z",
+    "createdAt": "2026-05-11T06:21:13.327Z"
+  },
+  {
+    "wallet": "0x9cb990f1862568a63d8601efeebe0304225c32f2",
+    "walletName": "0x9cb9...32f2",
+    "enabled": false,
+    "mode": "sign",
+    "amountUsd": 1,
+    "maxPriceDiff": 0.05,
+    "note": "",
+    "updatedAt": "2026-05-08T15:51:29.997Z",
+    "createdAt": "2026-05-07T20:45:12.865Z"
+  },
+  {
+    "wallet": "0xa38e7869b24dc48bb4216fb48fe86dbdfcbb8917",
+    "walletName": "0xa38e...8917",
+    "enabled": false,
+    "mode": "live",
+    "amountUsd": 1.2,
+    "maxPriceDiff": 0.05,
+    "note": "",
+    "updatedAt": "2026-05-07T20:24:06.830Z",
+    "createdAt": "2026-05-07T09:46:19.687Z"
+  },
+  {
+    "wallet": "0xcc500cbcc8b7cf5bd21975ebbea34f21b5644c82",
+    "walletName": "0xcc50...4c82",
+    "enabled": false,
+    "mode": "sign",
+    "amountUsd": 1.5,
+    "maxPriceDiff": 0.05,
+    "note": "",
+    "updatedAt": "2026-05-07T10:10:42.775Z",
+    "createdAt": "2026-05-06T18:00:55.753Z"
+  },
+  {
+    "wallet": "0x9495425feeb0c250accb89275c97587011b19a27",
+    "walletName": "LaBradfordSmith22",
+    "enabled": true,
+    "mode": "live",
+    "amountUsd": 4,
+    "maxPriceDiff": 0.065,
+    "note": "",
+    "updatedAt": "2026-05-11T03:06:24.710Z",
+    "createdAt": "2026-05-08T00:34:49.306Z"
+  },
+  {
+    "wallet": "0x9f2fe025f84839ca81dd8e0338892605702d2ca8",
+    "walletName": "surfandturf",
+    "enabled": true,
+    "mode": "live",
+    "amountUsd": 1.06,
+    "maxPriceDiff": 0.065,
+    "note": "",
+    "updatedAt": "2026-05-11T05:01:39.526Z",
+    "createdAt": "2026-05-11T05:00:42.957Z"
+  }
+]
